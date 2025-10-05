92451f5965ac70df71fbb8b07e3e18e7dd6da658
“Suyash-1011” <suyash1510s@gmail.com>
2025-10-06 00:28:13 +0530

    added ndk & cmake edits synced

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-
+    implementation(project(":sdk"))
     implementation("androidx.camera:camera-core:1.3.0")
     implementation("androidx.camera:camera-camera2:1.3.0")
     implementation("androidx.camera:camera-lifecycle:1.3.0")
